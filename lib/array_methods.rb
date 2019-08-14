afb19ae8bf0251207ae9e41c3986739469b9b940
Emily Harber <emily.a.harber@gmail.com>
2019-08-14 00:30:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   
   array.length.times do |x|
-    if array[x] == value_to_find then p array[x]
+    if array[x] === value_to_find then p array[x]
     end
   end
   
